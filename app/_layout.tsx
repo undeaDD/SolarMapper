0be262567d9ed8e7f6c2019278269517343a9ffd
Dominic Drees <undeaD_D@live.de>
2025-08-15 00:18:11 +0200

    optimize app configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
 import { Slot } from "expo-router";
 import React from "react";
-import { StatusBar } from "react-native";
+import { LogBox, StatusBar } from "react-native";
 
 import { DocumentProvider } from "@/provider/DocumentProvider";
 import { EllipsesProvider } from "@/provider/EllipseProvider";
 import { GlobalStateProvider } from "@/provider/GlobalStateProvider";
 import { ToolProvider } from "@/provider/ToolProvider";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { enableFreeze, enableScreens } from "react-native-screens";
+
+LogBox.ignoreAllLogs(true);
+enableFreeze(true);
+enableScreens(true);
 
 export default function Layout() {
   return (
